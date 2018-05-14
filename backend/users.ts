67d27c7cdd0e4270f23b773b7dae9d4a5ade4d94
João Paulo Cordeiro <jpc.araujo@hotmail.com>
2018-05-14 19:48:45 -0300

    Update users.ts

--- backend/users.ts
+++ backend/users.ts
@@ -12,7 +12,7 @@ export class User{
     }
 }
 
-export const users: {[key:string] : User} = {
-    "1@teste.com": new User("1@teste.com","teste1","teste1"),
-    "2@teste.com": new User("2@teste.com","teste2","teste2")
-}
+exports.users = {
+    "admin@teste.com": new User("admin@teste.com", "Admin", "teste"),
+    "usuario@teste.com": new User("usuario@teste.com", "Usuario", "teste")
+};
